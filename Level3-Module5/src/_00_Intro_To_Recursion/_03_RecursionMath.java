3adcb5a00d4211a5534e1780f8d6444dc0d6bc5d
Daniel Commins <dencee@users.noreply.github.com>
2021-06-24 16:37:18 -0700

    L3M5 Recursive math solutions

--- Level3-Module5/src/_00_Intro_To_Recursion/_03_RecursionMath.java
+++ Level3-Module5/src/_00_Intro_To_Recursion/_03_RecursionMath.java
@@ -8,24 +8,33 @@ public class _03_RecursionMath {
 
         // Else return number + recursionMultiplication(number, times-1)
 
-        if(times==1) {
+        if( times==1 ) {
             return number;
-        } else {
-            return number + recursiveMultiplication(number, times-1);
         }
+        
+        // Multiplication as a loop of addition
+        return number + recursiveMultiplication(number, times - 1);
     }
     
     // Try this one on your own! 
     // Hint: if numberToDivideBy is bigger than number,
     //       you can't divide anymore
     public static int recursiveDivision(int number, int numberToDevideBy) {
-        
-        return 0;
+        if( numberToDevideBy > number ) {
+            return 0;
+        }
+
+        // Division as a loop of subtraction
+        return 1 + recurDiv( number - numberToDevideBy, numberToDevideBy );
     }
 
     // Try this one on your own!
     public static int recursivePower(int number, int power) {
+        if( power == 0 ) {
+            return 1;
+        }
         
-        return 0;
+        // Power as a loop of multiplication
+        return number * recursivePower(number, power - 1);
     }
 }
