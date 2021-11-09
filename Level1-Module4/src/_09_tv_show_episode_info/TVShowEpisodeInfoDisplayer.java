60cd24b5adce1a19f9dc69fa7ff6d0d32e0d7aa6
Your Name <you@example.com>
2021-11-09 09:55:34 -0800

    added tv show info solution

---
+++ Level1-Module4/src/_09_tv_show_episode_info/TVShowEpisodeInfoDisplayer.java
@@ -0,0 +1,93 @@
+package _09_tv_show_episode_info;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public class TVShowEpisodeInfoDisplayer implements ActionListener {
+	JFrame window = new JFrame("TV Show Info");
+	JTextField field = new JTextField(20);
+	JButton button = new JButton("Submit");
+	public TVShowEpisodeInfoDisplayer() {
+		button.addActionListener(this);
+		window.setLayout(new FlowLayout());
+		window.add(field);
+		window.add(button);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+		window.pack();
+	}
+
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String v = getShowEpisodeData(field.getText());
+		JOptionPane.showMessageDialog(null, v);
+	}
+	
+
+/////////////////////////DO NOT MODIFY ANY CODE BELOW THIS LINE//////////////////////////////////////////
+	
+	/**
+	 * Searches the tvmaze.com api for season and episode information about
+	 * a chosen show and returns the information in a String
+	 * 
+	 * @param showTitle	the name of the show to get information about
+	 * @return 			a String containing the show information or a blank
+	 * 					String if the show is not found
+	 */
+	public String getShowEpisodeData(String showTitle) {
+		int id = WebUtilities.getShowId(showTitle);
+		if(id < 0) {
+			return "";
+		}
+		
+		int totalSeasons = 0;
+		int totalEpisodes = 0;
+		int[] episodes = null;
+		
+		try {
+			URL url = new URL("https://api.tvmaze.com/shows/"+id+"/seasons");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			InputStream responseStream = connection.getInputStream();
+			String in = WebUtilities.getStringFromInputStream(responseStream);
+			String[] info = WebUtilities.parseJSONArray(in);
+			totalSeasons = info.length;
+			episodes = new int[totalSeasons];
+			boolean success = true;
+			for(int i = 0; i < totalSeasons; i++) {
+				try {
+					episodes[i] = WebUtilities.getIntFromJSONObject(info[i], "episodeOrder");
+				}catch(Exception e) {
+					episodes[i] = -1;
+					success = false;
+				}
+				totalEpisodes += episodes[i];
+			}
+			if(!success) {
+				JOptionPane.showMessageDialog(null, "Could not retrieve some or all of the episode data.", "Data Not Available", 0);
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		String res = showTitle + "\nTotal Seasons: " + totalSeasons + "\nTotal Episodes: " + totalEpisodes + '\n';
+		for(int i = 0; i < totalSeasons; i++) {
+			res += "Season " + (i + 1) +": " + (episodes[i] > -1 ? episodes[i] : "?") + " episodes\n";
+		}
+		
+		return res;
+	}
+}
